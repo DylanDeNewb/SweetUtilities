19e7f66fdb8dba17ab5fba862da8d592c71eb769
DylanDeNewb <strawdylan1@gmail.com>
2022-03-22 20:43:29 +0000

    Initial Commit

---
+++ SweetUtilities/src/main/java/fun/sweetsmp/sweetutilities/SweetUtilities.java
@@ -0,0 +1,25 @@
+package fun.sweetsmp.sweetutilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+@Plugin(name = "SweetUtilities", version = "0.1.0")
+@Description("ExcellentCrates addon for Crate Key Upgrades")
+@Author("DylanDeNewb")
+@ApiVersion(ApiVersion.Target.v1_18)
+public final class SweetUtilities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
